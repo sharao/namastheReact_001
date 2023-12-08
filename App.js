ec97855537dd867555915cb8ff5d16d80a1f5c11
sharao <sharao111@gmail.com>
2023-12-08 11:28:49 +0530

    day one project init

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+ 'h1',
+ { id: 'heading' },
+ 'Hello world from JavaScript!!',
+);
+console;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
